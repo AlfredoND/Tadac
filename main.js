6b7648eae3264d135047d2a40a210c1635fc9da4
ANcode <alfayomega5027@gmail.com>
2024-08-26 20:24:52 -0400

    Create product section and add new style to About section

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const button = document.querySelector('.button')
-const nav    = document.querySelector('.nav')
+const nav = document.querySelector('.nav')
+const buttonAbout = document.querySelector('.button_about')
+const buttonProduct = document.querySelector('.button_product')
 
-button.addEventListener('click',()=>{
+button.addEventListener('click', () => {
     nav.classList.toggle('activo')
 })
+buttonAbout.addEventListener('click', () => {
+    alert("esta función no esta disponible ahora ")
+})
 
+buttonProduct.addEventListener('click', () => {
+    alert("No alejes tu vista  !que te pierdes su brillo¡")
+})
